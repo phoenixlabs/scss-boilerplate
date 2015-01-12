21fc3c00ec5cc8cf6fcefa577490a034bf7be9af
Maximilian Mayer <maximilianmayer.net@gmail.com>
2015-01-12 22:45:08 +0100

    [TASK] Switch from grunt to gulp and change the folder structure

--- Readme.md
+++ Readme.md
@@ -1,11 +1,11 @@
 # SCSS Boilerplate
 build from [Maximilian Mayer](http://twitter.com/maximilianmayer/) and [Rene Kreupl](http://www.renekreupl.de/)
 
-## Install the grunt dependencies
+## Install the gulp dependencies
 	# brings in all dependencies unless NODE_ENV === 'production' or --production flag is set
 	npm install
 
 ## Getting Started
 - Change "name" & "version" in "package.json"
-- Run "grunt" in your console for grunt-watch task
-- Run "grunt build" if you want combine media-queries
+- Run "gulp build" if you want compile the project files
+- Run "gulp watch" in your console for gulp-watch task
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp         = require('gulp'),
+	sass         = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssmin       = require('gulp-cssmin'),
+	concat       = require('gulp-concat'),
+	uglify       = require('gulp-uglify'),
+	order        = require('gulp-order'),
+	watch        = require('gulp-watch'),
+	livereload   = require('gulp-livereload'),
+	notify       = require('gulp-notify');
+
+
+gulp.task('sass', function() {
+	gulp.src('src/scss/*.scss')
+		.pipe(sass({
+			onError: function(err) {
+				return notify().write(err);
+			}
+		}))
+		.pipe(autoprefixer("last 2 version", "ie 9"))
+		.pipe(cssmin())
+		.pipe(gulp.dest('dist/css'));
+});
+
+
+gulp.task('js', function() {
+	gulp.src('src/js/main.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify({mangle: false}))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('vendor', function() {
+	gulp.src('src/js/vendor/*')
+		.pipe(gulp.dest('dist/js/vendor'));
+});
+
+
+gulp.task('watch', function() {
+	livereload.listen();
+
+	gulp.watch('src/scss/**/*.scss', ['sass']).on('change', livereload.changed);
+	gulp.watch('src/js/**/*.js', ['js']).on('change', livereload.changed);
+	gulp.watch('./**/*.html').on('change', livereload.changed);
+});
+
+
+gulp.task('build', ['sass', 'js', 'vendor']);
